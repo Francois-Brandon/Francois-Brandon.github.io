94346ff4039b567e48489997dfd02ba8dbe8f2d6
Brandon Francois <bafrancois@gmail.com>
2018-01-19 15:07:27 -0700

    Changed direction

--- topic1/script.js
+++ topic1/script.js
@@ -1,14 +1,3 @@
 function forLoop() {
-  var message = document.getElementById('formessage').value;
-  var num = Number(document.getElementById('loopnum').value);
-
-  console.log(message);
-  console.log(num);
-
-  for (var i = 0; i < num; i++) {
-    message += message + "<br>";
-    console.log(message);
-  }
-
-  document.getElementById("forresult").innerHTML = message;
+  
 }
