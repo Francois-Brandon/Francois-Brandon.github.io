8c60906454983d994dbbc7605988a181f189d661
Brandon Francois <bafrancois@gmail.com>
2018-03-07 07:57:38 -0700

    Fixing web service request

--- topic4/js-topic4.js
+++ topic4/js-topic4.js
@@ -7,9 +7,9 @@ function getTime() {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("response").innerHTML = this.responseText;
             document.getElementById("status").innerHTML = this.status;
-            document.getElementById("statustext").innerHTML = this.statusText;
+            //document.getElementById("statustext").innerHTML = this.statusText;
         }
     };
-    xhttp.open("GET", "http://date.jsontest.com", true);
+    xhttp.open("GET", "https://jsonplaceholder.typicode.com/posts/1", true);
     xhttp.send();
 }
