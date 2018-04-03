9aac9c7aa669e4fd75e2dec457776b1b08258930
Brandon Francois <bafrancois@gmail.com>
2018-04-03 15:55:17 -0700

    Save/Retrieve Schedule
    Added ability to save schedule to db and retrieve on schedule page

---
+++ whatsforlunch/js-whatsforlunch-schedule.js
@@ -0,0 +1,73 @@
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    var user = firebase.auth().currentUser;
+    var name = user.displayName;
+    var welcome = document.getElementById("welcome");
+    welcome.innerHTML = "Hello, " + name;
+      
+    var status = document.getElementById("login-status");    
+    status.setAttribute("onClick", "logOut()");
+    status.innerHTML = "Sign Out"
+ 
+    
+  } else {
+    var welcome = document.getElementById("welcome");
+    welcome.innerHTML = "Not logged in";
+      
+    var status = document.getElementById("login-status");
+    status.setAttribute("href", "login.html");
+    status.innerHTML = "Sign In"
+  }
+});
+
+var document = this.document;
+
+    
+function logOut() {
+    firebase.auth().signOut().then(function() {
+        console.log("signed out");
+    }).catch(function(error) {
+        console.log("error signing out");
+    });
+    
+    toggleDrop();
+}
+
+function getSchedule() {
+    
+    var user = firebase.auth().currentUser;
+    var uid = user.uid;
+    var docRef = db.collection("users").doc(uid);
+
+    docRef.get().then(function(doc) {
+    if (doc.exists) {
+        console.log("Document data:", doc.data().Monday);
+        document.getElementById("rest-mon").innerHTML = doc.data().Monday;
+        document.getElementById("rest-tues").innerHTML = doc.data().Tuesday;
+        document.getElementById("rest-wed").innerHTML = doc.data().Wednesday;
+        document.getElementById("rest-thurs").innerHTML = doc.data().Thursday;
+        document.getElementById("rest-fri").innerHTML = doc.data().Friday;
+        
+        var x = document.getElementById("schedule-page-container");
+        x.classList.add("show-schedule");
+
+        
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+    }).catch(function(error) {
+    console.log("Error getting document:", error);
+    });
+      
+}
+
+function toggleDrop() {
+    var x = document.getElementById("dropmenu");
+    if (x.classList.contains("droppeddown")) {
+        x.classList.remove("droppeddown");
+    } else {
+        x.classList.add("droppeddown");
+    }
+}
